b283c414a0bb4a95293b3f8fddc5339aecbd341b
Heitor Belém <heitorlbelem98@gmail.com>
2024-01-13 01:10:24 -0300

    Add new fonts to the project

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,8 +7,19 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
+    :root {
+        font-size: 62.5%;
+    }
+
     body {
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800 }
+    }
+
+    body, button, input, textarea { 
+        font-family: 'Roboto Slab', serif;
+        font-size: 1.6rem;
+        outline: none;
+        -webkit-font-smoothing: antialiased;
     }
 
     a {

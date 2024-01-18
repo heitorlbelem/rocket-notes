b295e2af79e1a97272f0d50342500c95a317ba55
Heitor Belém <heitorlbelem98@gmail.com>
2024-01-18 01:10:26 -0300

    Adds new tags to details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 import { Container, Links } from "./styles";
 
 export function Details() {
@@ -17,6 +18,11 @@ export function Details() {
             <a href="#">https://www.rocketseat.com.br</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
       <Button title="Voltar" />
     </Container>

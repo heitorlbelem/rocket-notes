d153e90e4f463e0f7b7891fa4e716dd4431b97cd
Heitor Belém <heitorlbelem98@gmail.com>
2024-01-18 00:59:08 -0300

    Changes all from px to rem

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   height: 100vh;
 
   display: grid;
-  grid-template-rows: 105px auto;
+  grid-template-rows: 10.5rem auto;
   grid-template-areas:
     "header"
     "content";

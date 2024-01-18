ae53852abc3dc6fdf4467e3a7503caa932f0cef6
Heitor Belém <heitorlbelem98@gmail.com>
2024-01-18 00:36:03 -0300

    Creates profile container in header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,16 @@
-import { Container } from './styles'
+import { Container, Profile } from "./styles";
 
 export function Header() {
-    return(
-        <Container>
-        </Container>
-    )
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/heitorlbelem.png" alt="Foto do Usuário" />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Heitor Belém</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }

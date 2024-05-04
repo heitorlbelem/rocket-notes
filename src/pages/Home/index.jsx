4ac8f4f6cdffe2ae90a5fb8ab1bc7ae5f7a1da9d
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-04 09:50:04 -0300

    Cria página Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import { Brand, Container, Menu, Content, NewNote, Search } from './styles'
+import { Header } from '../../components/Header';
+
+export function Home() {
+  return(
+    <Container>
+      <Brand>
+        <h1> RocketNotes </h1>
+      </Brand>
+      <Header />
+
+      <Menu></Menu>
+      <Search></Search>
+      <Content></Content>
+      <NewNote></NewNote>
+
+    </Container>
+  );
+}

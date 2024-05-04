273c96b60098b2540069ec38876db8b989a7e6fd
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-04 10:45:54 -0300

    Cria componente de input text

---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+  padding: 2rem 1.6rem;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  border-radius: 8px;
+
+  > input {
+    width: 100%;
+
+    font-size: 1.4rem;
+    
+    background: transparent;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    border: 0;
+    
+    &::placeholder {
+      opacity: 0.9;
+    }
+  }
+`;

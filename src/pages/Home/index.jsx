b9e1ce120d10c59797f784b91b2896eea3a2a162
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-04 11:44:15 -0300

    Adiciona componente note na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
-import { Brand, Container, Menu, Content, NewNote, Search } from './styles'
+import { Brand, Container, Menu, Content, NewNote, Search } from './styles';
 import { Header } from '../../components/Header';
-import { TextButton } from '../../components/TextButton'
+import { TextButton } from '../../components/TextButton';
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { Note } from '../../components/Note';
 
 export function Home() {
   return(
@@ -22,7 +24,22 @@ export function Home() {
       <Search>
         <Input placeholder='Pesquisar pelo título' icon={FiSearch}/>
       </Search>
-      <Content></Content>
+
+      <Content>
+        <Section title='Minhas notas'>
+          <Note data={{ title: 'React Modal', tags: [{id: 1, name: 'React'}] }} />
+          <Note
+            data={
+              {
+                title: 'Exemplo de Middleware',
+                tags: [{ id: 2, name: 'express' }, { id: 3, name: 'nodejs' }]
+              }
+            }
+          />
+        </Section>
+      </Content>
+
+
       <NewNote>
         <FiPlus size={24} />
         Criar nota

95ab176f5aae14415ecbb387c32b8404cc52b94f
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-05 02:32:11 -0300

    cria as rotas do app e de autenticação

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { NewNote } from '../pages/NewNote';
+import { Home } from '../pages/Home';
+import { Details } from '../pages/Details';
+import { Profile } from '../pages/Profile';
+
+
+export function AppRoutes() {
+  return(
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/details/:id' element={<Details />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/new' element={<NewNote />} />
+    </Routes>
+  )
+}

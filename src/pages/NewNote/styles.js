1ad5ce10c23aa5f87c46b5638195156920a97b93
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-04 21:37:39 -0300

    Cria página NewNote com header

---
+++ src/pages/NewNote/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+
+  grid-template-rows: 10.5rem auto;
+  grid-template-areas: "header" "content";
+`

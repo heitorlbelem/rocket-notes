f7fe516329ba35e08b23ab555d7aa94bd84bff88
Heitor Belém <heitorlbelem98@gmail.com>
2024-05-05 01:35:46 -0300

    Inicia componente do form de cadastro de nota

--- src/pages/NewNote/index.jsx
+++ src/pages/NewNote/index.jsx
@@ -1,10 +1,25 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
 
 export function NewNote(){
   return(
     <Container>
       <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href='#'>voltar</a>
+          </header>
+          
+          <Input placeholder='Título' />
+
+          <Section title='Links úteis'>
+          </Section>
+        </Form>
+      </main>
     </Container>
   )
 }
--- src/pages/NewNote/styles.js
+++ src/pages/NewNote/styles.js
@@ -1,8 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: grid;
+  width: 100%;
+  height: 100vh;
 
+  display: grid;
   grid-template-rows: 10.5rem auto;
-  grid-template-areas: "header" "content";
+  grid-template-areas:
+    "header"
+    "content"
+  ;
+`
+
+export const Form = styled.form`
+  max-width: 55rem;
+  margin: 3.8rem auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 3.6rem;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+      font-size: 2rem;
+    }
+  }
 `
